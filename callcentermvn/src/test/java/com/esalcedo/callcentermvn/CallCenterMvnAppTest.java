187bfcfab7ac1253f5ee038b26554a03a76840ad
Elkin Salcedo <elkinsalcedo@github.com>
2018-01-15 23:54:38 -0500

    Project Structure
    Project Structure

---
+++ callcentermvn/src/test/java/com/esalcedo/callcentermvn/CallCenterMvnAppTest.java
@@ -0,0 +1,14 @@
+package com.esalcedo.callcentermvn;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CallCenterMvnAppTest 
+{
+	@Test
+	public void callCenterMvn() {
+		System.out.println("This is a test !!!");
+	}
+}
